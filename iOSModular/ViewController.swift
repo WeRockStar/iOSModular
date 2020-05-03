0b896459780654935d4f77e45452f4c215729fc3
WeRockStar <kotchaphan.m@gmail.com>
2020-05-03 12:29:10 +0700

    Initial commit

---
+++ iOSModular/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSModular
+//
+//  Created by Kotchaphan Muangsan on 3/5/20.
+//  Copyright © 2020 Kotchaphan Muangsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
